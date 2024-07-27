fb89e73524478b59e0eb6372294d9e2fe4eac2c5
Naza1re <mcarim@mail.ru>
2024-07-27 17:40:00 +0300

    init commit

---
+++ src/test/java/org/example/flightsbookingservice/FlightsBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.flightsbookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsBookingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
