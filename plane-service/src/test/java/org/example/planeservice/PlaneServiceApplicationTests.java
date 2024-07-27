0f9d6c110b22bfacf01aab326006fd6b039a2b81
Naza1re <mcarim@mail.ru>
2024-07-27 19:15:07 +0300

    dev commit

---
+++ plane-service/src/test/java/org/example/planeservice/PlaneServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.planeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaneServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
